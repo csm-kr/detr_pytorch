1897272fc9e48d7637ec7366f1e9744a657db664
csm-kr <csm8167@naver.com>
2022-11-15 17:14:26 +0900

    add utilsg

---
+++ train.py
@@ -0,0 +1,73 @@
+import math
+import os
+import sys
+from typing import Iterable
+
+import torch
+
+import util.misc as utils
+from datasets.coco_eval import CocoEvaluator
+from datasets.panoptic_eval import PanopticEvaluator
+import time
+from tqdm import tqdm
+
+
+def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
+                    train_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, max_norm: float = 0, opts=None, vis=None):
+    model.train()
+    criterion.train()
+    tic = time.time()
+
+    for idx, data in enumerate(tqdm(train_loader)):
+        samples, targets = data
+        samples = samples.to(device)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        outputs = model(samples)
+        loss_dict = criterion(outputs, targets)
+        weight_dict = criterion.weight_dict
+        losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
+
+        optimizer.zero_grad()
+        losses.backward()
+        if max_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+        optimizer.step()
+
+        toc = time.time()
+
+        for param_group in optimizer.param_groups:
+            lr = param_group['lr']
+
+        # for each steps
+        if (idx % opts.vis_step == 0 or idx == len(train_loader) - 1) and opts.rank == 0:
+            print('Epoch: [{0}]\t'
+                  'Step: [{1}/{2}]\t'
+                  'Loss: {loss:.4f}\t'
+                  'Learning rate: {lr:.7f} s \t'
+                  'Time : {time:.4f}\t'
+                  .format(epoch, idx, len(train_loader),
+                          loss=losses,
+                          lr=lr,
+                          time=toc - tic))
+
+            if vis is not None:
+                # loss plot
+                vis.line(X=torch.ones((1, 1)).cpu() * idx + epoch * train_loader.__len__(),  # step
+                         Y=torch.Tensor([losses]).unsqueeze(0).cpu(),
+                         win='train_loss',
+                         update='append',
+                         opts=dict(xlabel='step',
+                                   ylabel='Loss',
+                                   title='training loss for {}'.format(opts.name),
+                                   legend=['Total Loss']))
+
+    if opts.rank == 0:
+        save_path = os.path.join(opts.log_dir, opts.name, 'saves')
+        os.makedirs(save_path, exist_ok=True)
+
+        # checkpoint = {'epoch': epoch,
+        #               'model_state_dict': model.state_dict()}
+        #
+        # torch.save(checkpoint, os.path.join(save_path, opts.name + '.{}.pth.tar'.format(epoch)))
---
+++ utils.py
@@ -0,0 +1,41 @@
+import os
+import torch
+import torch.distributed as dist
+
+
+def init_for_distributed(rank, opts):
+
+    # 1. setting for distributed training
+    opts.rank = rank
+    local_gpu_id = int(opts.gpu_ids[opts.rank])
+    torch.cuda.set_device(local_gpu_id)
+    if opts.rank is not None:
+        print("Use GPU: {} for training".format(local_gpu_id))
+
+    # # 2. init_process_group
+    dist.init_process_group(backend='nccl',
+                            init_method='tcp://127.0.0.1:23456',
+                            world_size=opts.world_size,
+                            rank=opts.rank)
+
+    # if put this function, the all processes block at all.
+    torch.distributed.barrier()
+    # convert print fn iif rank is zero
+    setup_for_distributed(opts.rank == 0)
+    print(opts)
+    return
+
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop('force', False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
